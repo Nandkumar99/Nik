2774a7234e8645fb1957eaaaa0fb7820cd70d987
suresh raina <suresh.raina1723@gmail.com>
2021-10-26 12:34:26 +0530

    dd

--- src/test/java/NikhilDada/Nikhil.java
+++ src/test/java/NikhilDada/Nikhil.java
@@ -3,16 +3,21 @@ package NikhilDada;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.IOException;
 
 public class Nikhil {
     public static void main(String[] args) throws InterruptedException, IOException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
         WebDriverManager.chromedriver().setup();
         WebDriver driver;
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
 
-        driver.navigate().to("https://www.facebook.com/");
+        driver.get("https://www.facebook.com/");
+
+        driver.quit();
     }
 }
